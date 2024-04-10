d913986b6dcc71e69214058a20ba4eacc2a17aef
Aditya Chaurasia <1212aditya11@gmail.com>
2024-04-11 02:45:02 +0530

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
-// import Create from "./Pages/Create";
+import Create from "./Pages/Create";
 import Navbar from "./Components/Navbar.jsx";
-// import Nft from "./Pages/Nft.jsx";
-// import Transfer from "./Pages/Transfer.jsx";
-// import NftPage from "./Pages/NftPage.jsx";
+import Nft from "./Pages/Nft.jsx";
+import Transfer from "./Pages/Transfer.jsx";
+import NftPage from "./Pages/NftPage.jsx";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
@@ -12,10 +15,11 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" exact element={<Home />} />
-        {/* <Route path="/nft" element={<Nft />} /> */}
-        {/* <Route path="/transfer" element={<Transfer />} /> */}
-        {/* <Route path="/nft/:id/:id2" element={<NftPage />} /> */}
+        <Route path="/nft" element={<Nft />} />
+        <Route path="/transfer" element={<Transfer />} />
+        <Route path="/nft/:address/:index" element={<NftPage />} />
       </Routes>
+      <ToastContainer />
     </BrowserRouter>
   );
 }
