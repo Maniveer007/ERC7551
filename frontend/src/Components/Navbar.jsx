ef89718657ead5af5fe7b34dd7b4e497bb2892db
Maniveer007 <maniveer198@gmail.com>
2024-04-18 16:07:34 +0530

    connect wallet button

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -17,6 +17,8 @@ const Navbar = () => {
 
   const loadAddress = async () => {
     const provider = new ethers.BrowserProvider(window.ethereum);
+
+    await provider.send("eth_requestAccounts", []);
     const signer = await provider.getSigner();
     const add = await signer.getAddress();
     setaddress(add);
